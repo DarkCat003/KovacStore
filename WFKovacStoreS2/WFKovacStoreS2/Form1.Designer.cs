04cbfe076376cdf962aebdf254bc14a2c89c1340
Angel <ang12novi@gmail.com>
2023-03-12 20:37:32 -0500

    Añadi algunas cosas

--- WFKovacStoreS2/WFKovacStoreS2/Form1.Designer.cs
+++ WFKovacStoreS2/WFKovacStoreS2/Form1.Designer.cs
@@ -28,19 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
+            this.btMostrar = new System.Windows.Forms.Button();
+            this.btIngresar = new System.Windows.Forms.Button();
+            this.mtxFecha = new System.Windows.Forms.MaskedTextBox();
+            this.mtxPrecio = new System.Windows.Forms.MaskedTextBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.cbxMarca = new System.Windows.Forms.ComboBox();
+            this.cbxColor = new System.Windows.Forms.ComboBox();
+            this.cbxTipo = new System.Windows.Forms.ComboBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btMostrar
+            // 
+            this.btMostrar.Location = new System.Drawing.Point(208, 415);
+            this.btMostrar.Name = "btMostrar";
+            this.btMostrar.Size = new System.Drawing.Size(94, 29);
+            this.btMostrar.TabIndex = 33;
+            this.btMostrar.Text = "Mostrar";
+            this.btMostrar.UseVisualStyleBackColor = true;
+            // 
+            // btIngresar
+            // 
+            this.btIngresar.Location = new System.Drawing.Point(78, 415);
+            this.btIngresar.Name = "btIngresar";
+            this.btIngresar.Size = new System.Drawing.Size(94, 29);
+            this.btIngresar.TabIndex = 32;
+            this.btIngresar.Text = "Ingresar";
+            this.btIngresar.UseVisualStyleBackColor = true;
+            // 
+            // mtxFecha
+            // 
+            this.mtxFecha.Location = new System.Drawing.Point(151, 352);
+            this.mtxFecha.Mask = "00/00/0000";
+            this.mtxFecha.Name = "mtxFecha";
+            this.mtxFecha.Size = new System.Drawing.Size(151, 27);
+            this.mtxFecha.TabIndex = 31;
+            this.mtxFecha.ValidatingType = typeof(System.DateTime);
+            // 
+            // mtxPrecio
+            // 
+            this.mtxPrecio.Location = new System.Drawing.Point(151, 302);
+            this.mtxPrecio.Mask = "999";
+            this.mtxPrecio.Name = "mtxPrecio";
+            this.mtxPrecio.Size = new System.Drawing.Size(151, 27);
+            this.mtxPrecio.TabIndex = 30;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "S",
+            "M",
+            "L"});
+            this.comboBox4.Location = new System.Drawing.Point(151, 267);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(151, 28);
+            this.comboBox4.TabIndex = 29;
+            // 
+            // cbxMarca
+            // 
+            this.cbxMarca.FormattingEnabled = true;
+            this.cbxMarca.Items.AddRange(new object[] {
+            "Addidas",
+            "Nika",
+            "Puma",
+            "Sin Marca"});
+            this.cbxMarca.Location = new System.Drawing.Point(151, 233);
+            this.cbxMarca.Name = "cbxMarca";
+            this.cbxMarca.Size = new System.Drawing.Size(151, 28);
+            this.cbxMarca.TabIndex = 28;
+            // 
+            // cbxColor
+            // 
+            this.cbxColor.FormattingEnabled = true;
+            this.cbxColor.Items.AddRange(new object[] {
+            "Rojo",
+            "Azul",
+            "Verde",
+            "Naranja"});
+            this.cbxColor.Location = new System.Drawing.Point(151, 199);
+            this.cbxColor.Name = "cbxColor";
+            this.cbxColor.Size = new System.Drawing.Size(151, 28);
+            this.cbxColor.TabIndex = 27;
+            // 
+            // cbxTipo
+            // 
+            this.cbxTipo.FormattingEnabled = true;
+            this.cbxTipo.Items.AddRange(new object[] {
+            "Hoodie",
+            "Camiseta",
+            "Zapatillas",
+            "Pantalon",
+            "Gorra"});
+            this.cbxTipo.Location = new System.Drawing.Point(151, 165);
+            this.cbxTipo.Name = "cbxTipo";
+            this.cbxTipo.Size = new System.Drawing.Size(151, 28);
+            this.cbxTipo.TabIndex = 26;
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(151, 128);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(151, 27);
+            this.txbNombre.TabIndex = 25;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Yu Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(166, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 43);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "KOVAC";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(70, 341);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 38);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Fecha de\r\ningreso:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(85, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 19);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Precio:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(96, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 19);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Talla:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(85, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 19);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Marca:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(90, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 19);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Color:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(96, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 19);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Tipo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(71, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 19);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nombre:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WFKovacStoreS2.Properties.Resources.KovacLoco;
+            this.pictureBox1.Location = new System.Drawing.Point(77, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(89, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 34;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 512);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btMostrar);
+            this.Controls.Add(this.btIngresar);
+            this.Controls.Add(this.mtxFecha);
+            this.Controls.Add(this.mtxPrecio);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.cbxMarca);
+            this.Controls.Add(this.cbxColor);
+            this.Controls.Add(this.cbxTipo);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btMostrar;
+        private Button btIngresar;
+        private MaskedTextBox mtxFecha;
+        private MaskedTextBox mtxPrecio;
+        private ComboBox comboBox4;
+        private ComboBox cbxMarca;
+        private ComboBox cbxColor;
+        private ComboBox cbxTipo;
+        private TextBox txbNombre;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
     }
 }
---
+++ WFKovacStoreS2/WFKovacStoreS2/Mostrar.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WFKovacStoreS2
+{
+    partial class Mostrar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btActualizar = new System.Windows.Forms.Button();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.cbxSeleccion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtgvMostrar = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvMostrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btActualizar
+            // 
+            this.btActualizar.Location = new System.Drawing.Point(12, 341);
+            this.btActualizar.Name = "btActualizar";
+            this.btActualizar.Size = new System.Drawing.Size(94, 29);
+            this.btActualizar.TabIndex = 11;
+            this.btActualizar.Text = "Actualizar";
+            this.btActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Location = new System.Drawing.Point(482, 377);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(94, 29);
+            this.btBuscar.TabIndex = 10;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(351, 377);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(125, 27);
+            this.txtBuscar.TabIndex = 9;
+            // 
+            // cbxSeleccion
+            // 
+            this.cbxSeleccion.FormattingEnabled = true;
+            this.cbxSeleccion.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.cbxSeleccion.Items.AddRange(new object[] {
+            "Nombre",
+            "Tipo",
+            "Color",
+            "Marca",
+            "Talla"});
+            this.cbxSeleccion.Location = new System.Drawing.Point(194, 376);
+            this.cbxSeleccion.Name = "cbxSeleccion";
+            this.cbxSeleccion.Size = new System.Drawing.Size(151, 28);
+            this.cbxSeleccion.TabIndex = 8;
+            this.cbxSeleccion.Text = "Seleccione . . .";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Busqueda Avanzada:";
+            // 
+            // dtgvMostrar
+            // 
+            this.dtgvMostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvMostrar.Location = new System.Drawing.Point(12, 44);
+            this.dtgvMostrar.Name = "dtgvMostrar";
+            this.dtgvMostrar.RowHeadersWidth = 51;
+            this.dtgvMostrar.RowTemplate.Height = 29;
+            this.dtgvMostrar.Size = new System.Drawing.Size(776, 289);
+            this.dtgvMostrar.TabIndex = 6;
+            // 
+            // Mostrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btActualizar);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.cbxSeleccion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgvMostrar);
+            this.Name = "Mostrar";
+            this.Text = "Mostrar";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvMostrar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btActualizar;
+        private Button btBuscar;
+        private TextBox txtBuscar;
+        private ComboBox cbxSeleccion;
+        private Label label1;
+        private DataGridView dtgvMostrar;
+    }
+}
